be5214580132e6d52d0e8d26d84fb8c0f1bed4c9
sairajzero <sairaj0630@gmail.com>
2019-04-19 13:19:47 +0530

    Update v0.9

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 echo "----------Welcome to FLO-Whatsapp AutoGen----------"
 
+if [ $(whoami) != "root" ];then
+	echo "Permission Denied! Try sudo $0";
+	exit 1
+fi
+
 if [ -f start ]; then
 	echo "FLO-Whatsapp is already AutoGen"
 	echo "To start run :\n./start <server-password>"
@@ -8,14 +13,14 @@ if [ -f start ]; then
 fi
 
 echo "----------Installing TOR----------"
-sudo apt-get install tor
+apt-get install tor
 echo "----------Configuring Tor for FLO-Whatsapp----------"
 echo $PWD
-sudo cat <<EOT >> /etc/tor/torrc
+cat <<EOT >> /etc/tor/torrc
 HiddenServiceDir $PWD/.hidden_service/
 HiddenServicePort 8000 127.0.0.1:8000
 EOT
-sudo chmod 700 $PWD
+chmod 700 $PWD
 echo "----------Finished Configuring----------"
 echo "----------Creating Start script----------"
 cat > start << EOF
@@ -31,7 +36,7 @@ OA=\$(cat .hidden_service/hostname)
 zenity --info --text="Open link '\$OA:8000' in onion browser"
 wait
 EOF
-chmod u+x start
+chmod +x start
 echo "----------Finished AutoGen----------"
 echo "To start run :\n./start <server-password>"
 
